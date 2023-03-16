31e6556a2704fb14d42a21126db0ec448be3f3f2
Yurevych Nataliia <116509687+yurevych2@users.noreply.github.com>
2023-03-16 16:51:10 +0200

    Fix a bug

--- classes.py
+++ classes.py
@@ -17,10 +17,17 @@ class Lzw_compress:
             if i == len(text) - 1:
                 code.append(list(chrs_dict.keys())[list(chrs_dict.values()).index(current_chr)]) # get the code
                 break
+            end = False
             while current_chr in chrs_dict.values():
                 i += 1
+                if i == len(text):
+                    code.append(list(chrs_dict.keys())[list(chrs_dict.values()).index(current_chr)]) # get the code
+                    end = True
+                    break
                 current_chr = current_chr + text[i]
-            current_chr = current_chr[0:-1]
+            if end:
+                break
+            current_chr = current_chr[0:-1] if len(current_chr) > 1 else current_chr
             code.append(list(chrs_dict.keys())[list(chrs_dict.values()).index(current_chr)]) # get the code
             next_chr = text[i]
             chrs_dict[len(chrs_dict)] = current_chr + next_chr
@@ -36,7 +43,7 @@ class Lzw_compress:
         text = []
         i = 0
         while i < len(code):
-            if i < len(code) - 1 and code[i+1] in default_dict.keys():
+            if i < len(code) - 1 and code[i+1] in default_dict:
                 default_dict[len(default_dict)] = default_dict[code[i]] + default_dict[code[i+1]][:1]
             else:
                 default_dict[len(default_dict)] = default_dict[code[i]] + default_dict[code[i]][:1]
