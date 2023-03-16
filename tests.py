145989778d1563f7b6d1edc079dd3c8e655526f1
Yurevych Nataliia <116509687+yurevych2@users.noreply.github.com>
2023-03-16 21:17:59 +0200

    Fix bug

--- tests.py
+++ tests.py
@@ -1,7 +1,16 @@
 from classes import Lz77_compress, Lzw_compress
 
 math_text = '11213409172489889898121341345134511617171167'
-weird_text = 'abacabadabacacacd'
+weird_text = 'abacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacd\
+abacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacd\
+abacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacd\
+abacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacd\
+abacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacd\
+abacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacd\
+abacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacd\
+abacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacd\
+abacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacd\
+abacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacdabacabadabacacacd'
 short_text = 'I love discrete math without any discussions.'
 big_text = 'Доброго дня, пані Світлано!\nЯ – … студентка Українського католицького \
 університету. Представниця організаторів інтелектуальної гри «Monty Quiz». Звертаюся \
@@ -32,5 +41,57 @@ for text in text_list:
     assert(text == Lz77_compress.decode(Lz77_compress.encode(text, 7)))
     assert(text == Lz77_compress.decode(Lz77_compress.encode(text, 8)))
     assert(text == Lz77_compress.decode(Lz77_compress.encode(text, 9)))
+
+# save text that is different
+with open('different_text.txt', 'r', encoding='utf-8') as file:
+    different_text = '\n'.join(file.readlines())
+
+# write encoded by LZW different text
+code, def_dict = Lzw_compress.encode(different_text)
+Lzw_compress.write_encoded(code, def_dict, 'different_encoded_lzw.txt')
+
+# write encoded by LZ-77 (buffer 7) different text
+code = Lz77_compress.encode(different_text, 7)
+Lz77_compress.write_encoded(code, 'different_encoded_lz77_7.txt')
+
+# write encoded by LZ-77 (buffer 17) different text
+code = Lz77_compress.encode(different_text, 17)
+Lz77_compress.write_encoded(code, 'different_encoded_lz77_17.txt')
+
+# write encoded by LZ-77 (buffer 27) different text
+code = Lz77_compress.encode(different_text, 27)
+Lz77_compress.write_encoded(code, 'different_encoded_lz77_27.txt')
+
+# save text that is similar
+with open('similar_text.txt', 'w') as file:
+    file.write(weird_text)
+
+# write encoded by LZW similar text
+code, def_dict = Lzw_compress.encode(weird_text)
+Lzw_compress.write_encoded(code, def_dict, 'similar_encoded_lzw.txt')
+
+# write encoded by LZ-77 (buffer 7) different text
+code = Lz77_compress.encode(weird_text, 7)
+Lz77_compress.write_encoded(code, 'similar_encoded_lz77_7.txt')
+
+# write encoded by LZ-77 (buffer 17) different text
+code = Lz77_compress.encode(weird_text, 17)
+Lz77_compress.write_encoded(code, 'similar_encoded_lz77_17.txt')
+
+# write encoded by LZ-77 (buffer 27) different text
+code = Lz77_compress.encode(weird_text, 27)
+Lz77_compress.write_encoded(code, 'similar_encoded_lz77_27.txt')
+
+# LZW
+# SIMILAR TEXT 833bytes before encoding and 456bytes after ~ 1,8
+# DIFFERENT TEXT 7,93KB before encoding and 11,5KB after ~ 0,68
+
+# LZ-77
+# SIMILAR TEXT with buffer 7 833bytes before encoding and 4,6KB after ~ 0,18
+# SIMILAR TEXT with buffer 17 833bytes before encoding and 714bytes after ~ 1,2
+# SIMILAR TEXT with buffer 27 833bytes before encoding and 2,51KB after ~ 0,33
+# DIFFERENT TEXT with buffer 7 7,93KB before encoding and 71,6 after ~ 0,12
+# DIFFERENT TEXT with buffer 17  7,93KB before encoding and 60,5KB after ~ 0,13
+# DIFFERENT TEXT with buffer 27  7,93KB before encoding and 55,4KB after ~ 0,14
 
 print('Nice work =^w^=')
